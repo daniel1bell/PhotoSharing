732db9892934f48b8e02ad521bc6c509777bda4a
ScottyGams <ScottyGammon@gmail.com>
2014-05-18 14:20:50 +0100

    adding user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+class UsersController < ApplicationController
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
+    @user.role = 'user'
+     if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+end
