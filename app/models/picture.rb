6b52fa5eff25cf30465e385d4ede92a8227d518c
paulvarga23 <paul_varga@me.com>
2014-05-18 21:31:22 +0100

    rmagick versions added

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,12 +2,14 @@ class Picture < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :altitude, :camera_make, :camera_model, :datetime, :exposure, :flash, :focal_length, :image, :image_height, :image_length, :latitude, :longitude, :name, :orientation, :picture_image, :tag_list
-  mount_uploader :picture_image
+  mount_uploader :picture_image, PictureImageUploader
 
   attr_accessible :altitude, :camera_make, :camera_model, :datetime, :exposure, :flash, :focal_length, :image, :image_height, :image_length, :latitude, :longitude, :name, :orientation
 
   acts_as_taggable
   acts_as_commentable
+
+  PictureImageUploader
   
 end
 
