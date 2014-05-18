1c0a6da7f678a5bc7663dee8a0854773faa04b87
daniel1bell <daniel1bell@me.com>
2014-05-18 21:14:33 +0100

    Edited incorrect column name in migration.

--- db/migrate/20140518195350_add_album_id_column_to_pictures.rb
+++ db/migrate/20140518195350_add_album_id_column_to_pictures.rb
@@ -1,6 +1,6 @@
 class AddAlbumIdColumnToPictures < ActiveRecord::Migration
   def self.up
-    remove_column :pictures, :user
+    remove_column :pictures, :user_id
     add_column :pictures, :album, :references
     
     add_index :pictures, :album_id
@@ -9,7 +9,7 @@ class AddAlbumIdColumnToPictures < ActiveRecord::Migration
 
   def self.down
     add_column :pictures, :user, :references
-    remove_column :pictures, :album
+    remove_column :pictures, :album_id
     
     add_index :pictures, :user_id
     remove_index  :pictures, :user_id
