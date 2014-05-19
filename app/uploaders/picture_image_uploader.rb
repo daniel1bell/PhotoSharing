fd4136f176b0a43ed454f6aaccc70699b57f5574
ScottyGams <ScottyGammon@gmail.com>
2014-05-19 14:27:41 +0100

    changing uploaded to use fog

--- app/uploaders/picture_image_uploader.rb
+++ app/uploaders/picture_image_uploader.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
 class PictureImageUploader < CarrierWave::Uploader::Base
-
+  include CarrierWave::MimeTypes
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -16,6 +16,8 @@ class PictureImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  process :set_content_type
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
