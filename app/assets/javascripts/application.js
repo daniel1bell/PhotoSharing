886887e8dc9dcf67f006b5a32052cf6eaa30bf7c
paulvarga23 <paul_varga@me.com>
2014-05-20 13:52:59 +0100

    contact page up and running

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,41 @@
 //= require foundation
 //= require_tree .
 
+function initialise() {
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.1),
+    zoom: 14,
+    streetViewControl: false,
+    mapTypeControl: false
+  };
+  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  addMarker(map);
+};
+
+
+function addMarker(map) {
+  console.log("Showing marker from geocoder results.");
+  var geocoder = new google.maps.Geocoder();
+  var showMarkerFromGeocoderResults = function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: map
+      });
+      map.setCenter(results[0].geometry.location);
+    } else {
+      console.warn("could not geocode address.");
+    }
+  }
+  $('address').each(function(i, el) {
+    var geocoderOptions = { address: $(el).text() };
+    geocoder.geocode(geocoderOptions, showMarkerFromGeocoderResults);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialise);
+
+
 $(function() {
   $(document).foundation();
 
