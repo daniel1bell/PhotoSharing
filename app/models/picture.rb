97259022bd2df83d6b5fda7303c43a72b6e63973
ScottyGams <ScottyGammon@gmail.com>
2014-05-21 14:21:50 +0100

    trying to save to database, ad added map to picture show page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,6 +4,7 @@ class Picture < ActiveRecord::Base
   attr_accessible :altitude, :camera_make, :camera_model, :datetime, :exposure, :flash, :focal_length, :image, :image_height, :image_length, :latitude, :longitude, :name, :orientation, :picture_image, :tag_list
   mount_uploader :picture_image, PictureImageUploader
   #after validation, save these items into the database. this would allow us to see pictures nearby by pulling them from the database.
+  before_save :save_exif
 
   attr_accessible :altitude, :camera_make, :camera_model, :datetime, :exposure, :flash, :focal_length, :image, :image_height, :image_length, :latitude, :longitude, :name, :orientation
 
@@ -25,6 +26,12 @@ class Picture < ActiveRecord::Base
       nil
     end
   end
+
+  # def save_exif
+  #     if picture_image.present?
+  #     :camera_model => self.exif_data.model
+  #     end
+  # end
 
   # @image.exif_data.date_time for example (in not nil or error will happen)
   
