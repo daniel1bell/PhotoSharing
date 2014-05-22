005e5be5d5e9e1dec9972d18df6e87c6541c9f16
daniel1bell <daniel1bell@me.com>
2014-05-22 23:14:21 +0100

    finished styling the home page with different tabs and rendering most recent, popular and trending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,17 @@
 class HomeController < ApplicationController
 
   def index
-    @recent_albums = [] << Album.most_recent.limit(6)
-    @popular_albums = [] << Album.most_liked << Album.most_commented
-    @trending_albums = [] << Album.most_liked << Album.most_commented << Album.most_recent
+    @recent_albums = Album.most_recent.limit(6)
+    @popular_albums = Album.most_liked + Album.most_commented
+    @trending_albums = Album.most_liked + Album.most_commented + Album.most_recent
 
-    @recent_pictures = [] << Picture.most_recent.limit(6)
-    @popular_pictures = [] << Picture.most_liked << Picture.most_commented
-    @trending_pictures = [] << Picture.most_liked << Picture.most_commented << Picture.most_recent
+    @recent_pictures = Picture.most_recent.limit(6)
+    @popular_pictures = Picture.most_liked + Picture.most_commented
+    @trending_pictures = Picture.most_liked + Picture.most_commented + Picture.most_recent
 
-    @recent_users = [] << User.most_recent.limit(4)
-    @popular_users = [] << User.most_liked << User.most_commented
-    @trending_users = [] << User.most_liked << User.most_commented << User.most_recent
+    @recent_users = User.most_recent.limit(4)
+    @popular_users = User.most_liked + User.most_commented
+    @trending_users = User.most_liked + User.most_commented + User.most_recent
 
   end
 
