2f42ce08bfda0ee45837f68ed062dab1bc27c6dc
paulvarga23 <paul_varga@me.com>
2014-05-22 18:59:41 +0100

    progress bar added for picture uploads

--- app/assets/javascripts/myapp.js
+++ app/assets/javascripts/myapp.js
@@ -7,8 +7,19 @@ myApp.toggleEmailSignup = function() {
 }
 
 
+
+myApp.showUpdating = function() {
+  $('.long_short').hide();
+  $('#updatemessage').slideDown();
+  $('#updatetext').slideDown();
+};
+
+
 myApp.setup = function() {
   $('#show_email_signin').on('click', myApp.toggleEmailSignup);
+  $('#picture_upload_button').on('click', myApp.showUpdating);
 }
+
+
 
 $(myApp.setup);
