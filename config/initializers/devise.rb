7a68b2ba368bb9f7a1910e7088337c03963a7155
ScottyGams <ScottyGammon@gmail.com>
2014-05-23 10:38:11 +0100

    now using ENV variables for all secret keys in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,8 +230,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, 'FACEBOOK_ID', 'FACEBOOK_KEY'
-  config.omniauth :twitter, 'TWITTER_ID', 'TWITTER_KEY'
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_KEY']
+  config.omniauth :twitter, ENV['TWITTER_ID'], ENV['TWITTER_KEY']
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
