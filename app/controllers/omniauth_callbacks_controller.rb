215baf54d08477653369967a0ca47781efca9781
ScottyGams <ScottyGammon@gmail.com>
2014-05-23 10:58:31 +0100

    adding some debug info to the flash message when users fail to create

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
-      flash.notice = "Problem creating account"
+      flash.notice = "Problem creating account because: #{user.errors.full_messages.to_sentence}"
       redirect_to new_user_registration_url
     end
   end
@@ -19,7 +19,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
-      flash.notice = "Problem creating account"
+      flash.notice = "Problem creating account because: #{user.errors.full_messages.to_sentence}"
       redirect_to new_user_registration_url
     end
   end
