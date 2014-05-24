bc032845dbb6597724074ca1d9a6d994944359e1
daniel1bell <daniel1bell@me.com>
2014-05-24 10:31:58 +0100

    updated homepage layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if params[:user][:password].blank?
+    if params[:user][:password] && params[:user][:password].blank?
       params[:user].delete(:password)
       params[:user].delete(:password_confirmation)
     end
